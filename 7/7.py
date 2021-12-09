22d7fa0ee405579b26e084fd770cfc9ade3b4bc0
Griffin Badalamente <gb678@cam.ac.uk>
2021-12-09 19:44:15 +0000

    day 7

---
+++ 7/7.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+with open('input.txt') as f:
+	lines = f.read().splitlines()
+
+pos = np.asarray(lines[0].split(','), dtype=int)
+#pos = np.asarray([16,1,2,0,4,2,7,1,2,14])
+fuel = []
+for i in np.arange(pos.min(), pos.max()):
+	fuel.append(sum(abs(pos-i)))
+print(f'min human fuel use is {min(fuel)}')
+
+fuel = []
+for i in np.arange(pos.min(), pos.max()):
+	fuel.append(sum([(abs(x-i)*(abs(x-i)+1))/2 for x in pos]))
+
+print(f'min crab fuel use is {int(min(fuel))}')
