f9458619d3a3d2933efc83b2ebc2eb5d471c5cb6
Griffin Badalamente <gb678@cam.ac.uk>
2021-12-17 19:58:45 +0000

    15 start

--- 15/15.py
+++ 15/15.py
@@ -5,4 +5,7 @@ with open('test.txt') as f:
 #with open('input.txt') as f:
 	lines = f.read().splitlines()
 
-print(lines)
+#print(lines)
+
+for i,line in enumerate(lines):
+	lines[i] = list(map(int, list(line)))
